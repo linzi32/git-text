a15187e1c816e6759212efd19d62fb04412fdbd5
xuelin <2933703031@qq.com>
2022-11-10 11:02:32 +0800

    变更

---
+++ src/main/java/com/git/gittext.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class gittext {
+    public static void main(String[] args) {
+       System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+
+
+
+    }
+
+
+}
